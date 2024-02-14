d846454452ff4a0b39ec83c3f93ccf58072ef3e1
Sacir <turksacir@hotmail.com>
2024-02-14 18:12:44 +0100

    prints out all data from database

--- src/main/java/com/example/shopbackend/controller/dataController.java
+++ src/main/java/com/example/shopbackend/controller/dataController.java
@@ -5,18 +5,24 @@ import com.example.shopbackend.repository.dataRepository;
 import com.example.shopbackend.repository.shopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/")
 public class dataController {
 
     @Autowired
     private dataRepository dataRep;
 
-    @PostMapping("/addItems")
+    @PostMapping("addItems")
     public shop shopAdd(@RequestBody shop shopAdd){
         return dataRep.save(shopAdd);
+    }
+
+    @GetMapping("showAll")
+    public List<shop> showData(){
+        return dataRep.allData();
     }
 }
--- src/main/java/com/example/shopbackend/repository/dataRepository.java
+++ src/main/java/com/example/shopbackend/repository/dataRepository.java
@@ -2,8 +2,16 @@ package com.example.shopbackend.repository;
 
 import com.example.shopbackend.model.shop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface dataRepository  extends JpaRepository<shop, Long> {
+
+    @Query(value = "select * from shop", nativeQuery = true)
+    List<shop> allData();
+
+
 }
